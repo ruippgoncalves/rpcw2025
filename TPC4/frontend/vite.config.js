116e69018b24f2d5ac04be6ba18f99ca7b1a94a8
ruippgoncalves <rgoncalves0320@gmail.com>
2025-02-26 21:36:16 +0000

    Hosts

--- TPC4/frontend/vite.config.js
+++ TPC4/frontend/vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    include: /\.js$/,
-    exclude: /node_modules/,
-    loader: 'jsx',
-  }
+    plugins: [react()],
+    esbuild: {
+        include: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'jsx',
+    },
+    /*server: {
+        allowedHosts: ['localhost'],
+    }*/
 })
